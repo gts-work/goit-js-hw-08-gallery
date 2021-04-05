57eb8fa39218eddc178891ed0a7fd33bfca52db3
Grygorii Tsaryk <artgrem@gmail.com>
2021-04-06 00:23:49 +0300

    did an additional task

--- js/gallery.js
+++ js/gallery.js
@@ -16,12 +16,14 @@ import images from './data/gallery-items.js';
   </a>
 </li> */
 
+// DOCUMENT ELEMENTS
 const galleryContainerEl = document.querySelector('.js-gallery');
 const galleryLightboxEl = document.querySelector('.js-lightbox');
 const closeButtonEl = document.querySelector('button[data-action="close-lightbox"]');
 const lightBoxImageEl = document.querySelector('.lightbox__image');
-const activeGalleryLink = '#';
+const lightBoxOverlayEl = document.querySelector('.lightbox__overlay');
 
+// CREATE GALLERY ITEMS
 function createImagesItemMarkup(obj) {
   return obj.map((image) => {
     const galleryItem = createItemList();
@@ -64,9 +66,13 @@ function createItemImage(obj) {
 const galleryBody = createImagesItemMarkup(images);
 galleryContainerEl.append(...galleryBody);
 
+// EVENTS
 galleryContainerEl.addEventListener('click', onShowImage);
 closeButtonEl.addEventListener('click', onCloseModal);
+lightBoxOverlayEl.addEventListener('click', onLightBoxClick);
+window.addEventListener('keydown', onKeyPress);
 
+// EVENTS FUNCTIONS
 function onShowImage(e) {
   const isImageEl = e.target.classList.contains('gallery__image');
   if (!isImageEl) {
@@ -82,6 +88,80 @@ function onShowImage(e) {
 }
 
 function onCloseModal(e) {
+  closeModal();
+}
+
+function closeModal() {
   galleryLightboxEl.classList.remove('is-open');
   lightBoxImageEl.src = '';
 }
+
+function onLightBoxClick(e) {
+  console.log(e.currentTarget);
+  console.log(e.target);
+  closeModal();
+}
+
+// GET ARRAY ORIGINAL IMAGES
+function getArrayImagesSrc(obj) {
+  const arrSrc = [];
+  obj.forEach(image => arrSrc.push(image.dataset.source));
+  return arrSrc;
+}
+
+function onKeyPress(e) {
+  // console.log(e.code);
+  // code: "ArrowRight"
+  // code: "ArrowLeft"
+
+  if (e.code != 'Escape' && e.code != 'ArrowRight' && e.code != 'ArrowLeft') {
+    return;
+  }
+
+  if (e.code === 'Escape') {
+    closeModal();
+    return;
+  }
+
+  if (e.code === 'ArrowRight') {
+    if (galleryLightboxEl.classList.contains('is-open')) {
+      const arrImageSrc = getArrayImagesSrc(document.querySelectorAll('.gallery__image'));
+      const currentIndexSrc = arrImageSrc.indexOf(lightBoxImageEl.src);
+      let nextIndexSrc = arrImageSrc[currentIndexSrc + 1];
+
+      if (!nextIndexSrc) {
+        nextIndexSrc = arrImageSrc[0];
+      }
+
+      // console.log('3 nextIndexSrc: ', nextIndexSrc);
+
+      lightBoxImageEl.src = '';
+      lightBoxImageEl.src = nextIndexSrc;
+
+      console.log('3: ', lightBoxImageEl.src);
+
+      return;
+    }
+  }
+
+  if (e.code === 'ArrowLeft') {
+    if (galleryLightboxEl.classList.contains('is-open')) {
+      // console.log('3: ', e.code);
+      const arrImageSrc = getArrayImagesSrc(document.querySelectorAll('.gallery__image'));
+      const currentIndexSrc = arrImageSrc.indexOf(lightBoxImageEl.src);
+      let nextIndexSrc = arrImageSrc[currentIndexSrc - 1];
+
+      if (!nextIndexSrc) {
+        nextIndexSrc = arrImageSrc[arrImageSrc.length - 1];
+      }
+
+      lightBoxImageEl.src = '';
+      lightBoxImageEl.src = nextIndexSrc;
+
+      console.log('3: ', lightBoxImageEl.src);
+
+      return;
+    }
+  }
+}
+
